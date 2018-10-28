5722d79a65a05322dcf489c81ab35c9866aaee14
Florian <florian-1992@hotmail.fr>
2018-10-28 10:39:34 +0100

    Initializing project tracking

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "std_lib_facilities.h"
+
+int main()
+{
+    std::string message;
+
+    std::cout << "Enter a message you want to crypt: " << std::endl;
+    return 0;
+}
