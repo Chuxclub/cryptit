9dd16c8f61ebe8600b8e8261fa54a4ffa92e2626
Florian <florian-1992@hotmail.fr>
2018-12-05 23:08:53 +0100

    Few tweaks done to code readibility

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 
 int main()
 {
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Caesar encrypting algorithm attempt  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
     char message = ' ';
-    std::string encrypted_message;
     vector<char> msg_vector;
+    constexpr int VECTOR_INDEX_GAP = 1;
     int pace = 0;
 
+
+
+    /* Message is taken character by character and placed in a vector.
+     * cin.get() allows to include whitespaces in the inline stream */
 
     std::cout << "Enter a message you want to crypt: ";
 
@@ -18,27 +24,42 @@ int main()
 
 
 
+    /* Pace is the gap between the first letter in the alphabet and the letter 'a'
+     * in the caesar alphabet sequence */
+
     std::cout << "Enter a pace (range 0 to 26): ";
     std::cin >> pace;
 
-    for (int i = 0; i < msg_vector.size(); i++)
-    {
-        if (msg_vector.at(i) > 97 && msg_vector.at(i) < 122 && msg_vector[i] + pace < 122)
-            msg_vector.at(i) = msg_vector[i] + pace;
 
-        else if (msg_vector.at(i) > 97 && msg_vector.at(i) < 122 && msg_vector[i] + pace > 122)
-            msg_vector.at(i) = (msg_vector[i] + pace)%122 + 96; //Revoir ce qu'il se passe avec modulo et index vecteur
+
+    /* Translate every char in the message vector according to the pace chosen by the user */
+
+    for (unsigned short int i = 0; i < msg_vector.size(); i++)
+    {
+        if (msg_vector.at(i) >= 'a' && msg_vector.at(i) <= 'z' && msg_vector[i] + pace < 'z')
+            msg_vector.at(i) = narrow_cast<char>(msg_vector[i] + pace);
+
+        else if (msg_vector.at(i) >= 'a' && msg_vector.at(i) <= 'z' && msg_vector[i] + pace > 'z')
+            msg_vector.at(i) = narrow_cast<char>((msg_vector[i] + pace)%'z' + 'a' - VECTOR_INDEX_GAP);
+
+        else if (msg_vector.at(i) >= 'A' && msg_vector.at(i) <= 'Z' && msg_vector[i] + pace < 'Z')
+            msg_vector.at(i) = narrow_cast<char>(msg_vector[i] + pace);
+
+        else if (msg_vector.at(i) >= 'A' && msg_vector.at(i) <= 'Z' && msg_vector[i] + pace > 'Z')
+            msg_vector.at(i) = narrow_cast<char>((msg_vector[i] + pace)%'Z' + 'A' - VECTOR_INDEX_GAP);
 
         else
             msg_vector.at(i) = msg_vector[i];
     }
 
 
+    /* Prints encrypted message */
+
     for (char x: msg_vector)
         std::cout << x;
 
 
     return 0;
-}
 
-// vector<char> min_alphabet['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+}
